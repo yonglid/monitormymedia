2f3a90c1682d533f819fdbd7db73114fae8efdd3
Emily <yongldich@gmail.com>
2020-05-12 00:44:44 -0700

    added eslint to enforce style for js and refactored codes; added babel for compiling earlier ecma; added nodemon to restart server for app changes

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const app = express();
 // set up port number
 const port = 5035;
 // set up home route
-app.get('/', (request, respond) => {
+app.get('/', (respond) => {
   respond.status(200).json({
     message: 'Welcome to Project Support',
   });
 });
-app.listen(port, (request, respond) => {
+app.listen(port, () => {
   console.log(`Our server is live on ${port}. Yay!`);
 });
