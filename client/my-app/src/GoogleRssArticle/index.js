c4efdaf8fda3b8d3c7e75456fabcdf5e18cf6f71
Andrew Glass <andrewglass1@gmail.com>
2020-06-09 16:23:14 -0400

    style cards

--- client/my-app/src/GoogleRssArticle/index.js
+++ client/my-app/src/GoogleRssArticle/index.js
@@ -6,9 +6,9 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link'; 
+import Link from '@material-ui/core/Link';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import StarIcon from '@material-ui/icons/Star'; 
+import StarIcon from '@material-ui/icons/Star';
 import Button from '@material-ui/core/Button';
 
 
@@ -35,22 +35,19 @@ class GoogleRssArticle extends React.Component {
     const { classes } = this.props;
 
     return (
-      <Card className={classes.card}>
-        <CardHeader
-          title={this.props.article.title}
-        />
-        <CardContent>
-          
-          <Button variant="contained" color="inherit" href={this.props.article.link}>
-            View Article
-          </Button>
-        </CardContent>
+      <div className="article-card">
+        <h2>{this.props.article.title}</h2>
+
+
+        <Button variant="contained" color="inherit" href={this.props.article.link} target="_blank">
+          View Article
+        </Button>
         <CardActions className={classes.actions} disableActionSpacing>
           <IconButton aria-label="Add to favorites" onClick={this.handleClick}>
             <StarIcon color={this.props.isStarred ? "secondary" : "primary"} />
           </IconButton>
         </CardActions>
-      </Card>
+      </div>
     );
   }
 }
