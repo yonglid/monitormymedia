baef1744af5026ef3c53c754691229bde7a37048
Emily <yongldich@gmail.com>
2020-08-04 11:32:39 -0700

    changed feedbacks components to proper routes to components

--- client/my-app/src/FeedbackLanding/index.js
+++ client/my-app/src/FeedbackLanding/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import SearchBar from '../SearchBar';
+import SearchBar from '../components/SearchBar';
 
 const useStyles = makeStyles((theme) => ({
     root: {
